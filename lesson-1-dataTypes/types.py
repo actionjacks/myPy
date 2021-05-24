c52b04b89aba2b57797bd72e6af75a50fb40ef95
jacek zablocki <56847035+actionjacks@users.noreply.github.com>
2021-05-24 22:46:30 +0200

    first

---
+++ lesson-1-dataTypes/types.py
@@ -0,0 +1,22 @@
+number = 1
+print(type(number))
+
+number2 = 1.2
+print(type(number2))
+
+myArray = ['2', '32', 'dw', 1]
+print(type(myArray))
+
+print('nowa \n linia')
+
+# check lenght
+print(len('jacek'))
+
+mystring = 'jacek'
+print(mystring[2])
+print(mystring[-1])
+print(mystring[0:4])
+print(mystring[:3])
+# stop on 3index and dont include
+print(mystring[::3])
+# take every 3rd index
